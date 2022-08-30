45e1ed42b4a7be610588854a2a5e3eef0ad6e2c0
mathdeng <75908296+mathdeng@users.noreply.github.com>
2022-08-30 09:10:27 +0800

    更新

--- js/CMFU.js
+++ js/CMFU.js
@@ -6,6 +6,6 @@ let 文本 = "";
 for (const [键 , 值] of Object.entries(CMFU)) {
     文本 += `<li><a href = "https://m.qidian.com/book/${键}.html">${值}</a></li>`
 }
-文本 += `<ul>${文本}</ul>`;
+文本 = `<ul>${文本}</ul>`;
 
 document.getElementById("CMFU").innerHTML = 文本;
