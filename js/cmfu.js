123f995cf3266ee6242d363ee482973b36b04114
mathdeng <75908296+mathdeng@users.noreply.github.com>
2022-09-10 18:36:46 +0800

    更新

--- js/cmfu.js
+++ js/cmfu.js
@@ -36,7 +36,7 @@ const cmfu = {
 
 let 文本 = "";
 
-for (const 键 of Object.keys(cmfu).sort((a , b) => a - b)) {
+for (const 键 of Object.keys(cmfu).sort((a , b) => Number(a) - Number(b))) {
     文本 += `<li><a href = "https://m.qidian.com/book/${键}.html">${cmfu[值]}</a></li>`
 }
 文本 = `<ul>${文本}</ul>`;
