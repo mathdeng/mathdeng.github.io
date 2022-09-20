287cedd28992c8a52038a8baf57ef6b07cb41d57
mathdeng <75908296+mathdeng@users.noreply.github.com>
2022-09-20 19:24:32 +0800

    更新

--- js/首.js
+++ js/首.js
@@ -1,1 +1,1 @@
-document.getElementById("首").innerHTML = `<p><a href = "https://www.mathdeng.top/">Top</a>&emsp;<a href = "https://mathdeng.github.io/">Page</a></p>`;
+document.getElementById("首").innerHTML = `<p style = "text-align : right"><a href = "https://www.mathdeng.top/">Top</a>&emsp;<a href = "https://mathdeng.github.io/">Page</a></p>`;
