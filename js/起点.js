abc128aeb5f401efddf24e1d2d75b375162d1166
mathdeng <75908296+mathdeng@users.noreply.github.com>
2022-12-09 23:59:58 +0800

    更新

--- js/起点.js
+++ js/起点.js
@@ -11,6 +11,7 @@ const cmfu = {
     "https://m.qidian.com/book/1033516786.html":"从机械猎人开始",
     "https://m.qidian.com/book/1034869678.html":"我有一个游戏世界",
     "https://m.qidian.com/book/1023715291.html":"旧日之箓",
+    "https://m.qidian.com/book/1035362595.html":"异化武道",
     "https://m.qidian.com/book/1034753010.html":"超维杀",
     "https://m.qidian.com/book/1035341423.html":"我不可能会录这种自述",
     "https://m.qidian.com/book/1035513072.html":"我成了见习神明",
