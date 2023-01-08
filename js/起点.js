5ea36dec881e1b7ac3c9aa3d40664591d8f43a3c
mathdeng <75908296+mathdeng@users.noreply.github.com>
2023-01-08 12:32:49 +0800

    Update 起点.js

--- js/起点.js
+++ js/起点.js
@@ -1,9 +1,6 @@
 const cmfu = {
-    "https://m.qidian.com/book/1035937745.html":"武道通神：我有一株武道果树",
     "https://m.qidian.com/book/1035882576.html":"家族修仙：从卖盒饭开始",
-    "https://m.qidian.com/book/1036037303.html":"师兄说得对",
     "https://m.qidian.com/book/1035700993.html":"新东方毕业，你让我烧大席？",
-    "https://m.qidian.com/book/1035852105.html":"玄德",
     "https://m.qidian.com/book/1035827370.html":"我武术宗师过分张狂，黑粉三千万",
     "https://m.qidian.com/book/1035785432.html":"导演从拯救家族产业开始"
 };
