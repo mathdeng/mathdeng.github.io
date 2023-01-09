0748e980db55bcb512d1607f409c263c04939999
mathdeng <75908296+mathdeng@users.noreply.github.com>
2023-01-09 11:56:56 +0800

    更新

--- js/起点.js
+++ js/起点.js
@@ -1,4 +1,5 @@
 const cmfu = {
+    "https://m.qidian.com/book/1035791379.html":"开局签到厨神灌汤包",
     "https://m.qidian.com/book/1035882576.html":"家族修仙：从卖盒饭开始",
     "https://m.qidian.com/book/1035700993.html":"新东方毕业，你让我烧大席？",
     "https://m.qidian.com/book/1035827370.html":"我武术宗师过分张狂，黑粉三千万",
