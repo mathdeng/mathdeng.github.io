c5153ec3002f2cbebab30bc4493915e4a44ac576
mathdeng <75908296+mathdeng@users.noreply.github.com>
2023-01-22 16:10:53 +0800

    更新

--- js/起点.js
+++ js/起点.js
@@ -1,6 +1,4 @@
 const cmfu = {
-    "https://m.qidian.com/book/1035700993.html":"新东方毕业，你让我烧大席？",
-    "https://m.qidian.com/book/1035827370.html":"我武术宗师过分张狂，黑粉三千万",
     "https://m.qidian.com/book/1035785432.html":"导演从拯救家族产业开始"
 };
 
