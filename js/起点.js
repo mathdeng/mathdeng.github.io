dd405c708558de342797ab6e2e71f323b7c9a75b
Forest Ye <ezhou10@outlook.com>
2024-03-30 00:49:02 +0000

    更新

--- js/起点.js
+++ js/起点.js
@@ -5,7 +5,7 @@ function 表情(星级) {
 
 let 文本 = '';
 for (const 元素 of qidian) {
-    文本 += `<li><a href = "${元素.fields.url}">${表情(元素.fields.书名)}</a></li>`;
+    文本 += `<li><a href = "${元素.fields.url}">${表情(元素.fields.星级) + 元素.fields.书名}</a></li>`;
 }
 文本 = `<ol>${文本}</ol>`;
 document.getElementById('起点').innerHTML = 文本;
