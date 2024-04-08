74a398b58980e9b292ed1abe8a8def9d37839551
ezhou10 <ezhou10@outlook.com>
2024-04-08 08:27:29 +0000

    更新

--- js/起点.js
+++ js/起点.js
@@ -3,9 +3,9 @@ function 表情(星级) {
     return 列表[Math.min(星级, 列表.length - 1)]
 }
 
-let 文本 = `<p>总共 ${ qidian.length } 本书。</p>`;
+let 文本 = `<p>总共 ${ qidian.length } 本书。</p><ol>`;
 for (const 元素 of qidian) {
     文本 += `<li><a href = "${元素.fields.url}">${表情(元素.fields.星级) + 元素.fields.书名}</a></li>`;
 }
-文本 = `<ol>${文本}</ol>`;
+文本 += `</ol>`;
 document.getElementById('起点').innerHTML = 文本;
